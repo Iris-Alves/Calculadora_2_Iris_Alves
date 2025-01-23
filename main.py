61ccf1911636b141e33a1ed2938c059ca748f1eb
Iris-Alves <iris.karagianis@gmail.com>
2025-01-23 12:05:27 +0000

    Calculadora Versão 2
    Alteração da Calculadora versão2

--- main.py
+++ main.py
@@ -25,13 +25,17 @@ def calculadora(num1: float, num2: float, operador: str) -> float:
 def calculadora_v2(num1: float, num2: float, operador: str) -> float:
     operacoes = {
         "+": lambda: num1 + num2,
+        "-": lambda: num1 - num2,
+        "/": lambda: num1 / num2,
+        "*": lambda: num1 * num2,
         "%": lambda: num1 % num2,
         "^": lambda: num1 ** num2,
     }
     funcao = operacoes.get(operador)
     if funcao:
         return funcao()
-
+    else:
+        print("Erro! Opção inválida! ")
     return float("nan")
 
 
